924a3a12586d1d3bb946d80c0f378cff64a5bc99
MASINGITA OTTIS MALULEKE <216135982@edu.vut.ac.za>
2026-01-14 15:58:33 +0200

    fix: Correct amplify_outputs.json import path from root directory

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 
   import { createRoot } from "react-dom/client";
   import { Amplify } from 'aws-amplify';
-  import outputs from './amplify_outputs.json';
+  import outputs from '../amplify_outputs.json';
   import App from "./App.tsx";
   import "./index.css";
 
