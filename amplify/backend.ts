f9ed7e9eb898ebb5aecda98119e9fec6c31906f6
MASINGITA OTTIS MALULEKE <216135982@edu.vut.ac.za>
2026-01-16 14:33:24 +0200

    Simplify backend.ts - remove complex CDK VPC lookup that was blocking deployment

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,11 +2,6 @@
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 import { functions } from './functions/resource';
-import { Stack } from 'aws-cdk-lib';
-import { HttpApi, HttpMethod, CorsHttpMethod } from 'aws-cdk-lib/aws-apigatewayv2';
-import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as iam from 'aws-cdk-lib/aws-iam';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -17,88 +12,8 @@ const backend = defineBackend({
   functions,
 });
 
-// Get the auth Lambda function
-const authLambda = backend.functions.auth.resources.lambda;
-
-// Add VPC configuration to Lambda
-const vpc = ec2.Vpc.fromLookup(Stack.of(authLambda), 'ExistingVPC', {
-  vpcId: 'vpc-09a5094b22e709eef',
-});
-
-const securityGroup = ec2.SecurityGroup.fromSecurityGroupId(
-  Stack.of(authLambda),
-  'ExistingSecurityGroup',
-  'sg-0655ac897b884d2ce'
-);
-
-// Add VPC config
-authLambda.addToRolePolicy(new iam.PolicyStatement({
-  effect: iam.Effect.ALLOW,
-  actions: [
-    'ec2:CreateNetworkInterface',
-    'ec2:DescribeNetworkInterfaces',
-    'ec2:DeleteNetworkInterface',
-    'ec2:AssignPrivateIpAddresses',
-    'ec2:UnassignPrivateIpAddresses'
-  ],
-  resources: ['*'],
-}));
-
-// Add Secrets Manager permissions
-authLambda.addToRolePolicy(new iam.PolicyStatement({
-  effect: iam.Effect.ALLOW,
-  actions: [
-    'secretsmanager:GetSecretValue',
-    'secretsmanager:DescribeSecret'
-  ],
-  resources: [
-    'arn:aws:secretsmanager:us-east-1:357229249466:secret:my-app/db-credentials-*',
-    'arn:aws:secretsmanager:us-east-1:357229249466:secret:my-app/jwt-secret-*'
-  ],
-}));
-
-// Add SES permissions
-authLambda.addToRolePolicy(new iam.PolicyStatement({
-  effect: iam.Effect.ALLOW,
-  actions: [
-    'ses:SendEmail',
-    'ses:SendRawEmail'
-  ],
-  resources: ['*'],
-}));
-
-// Create HTTP API Gateway for auth endpoints
-const apiStack = backend.createStack('http-api-stack');
-
-// Create the HTTP API
-const httpApi = new HttpApi(apiStack, 'AuthHttpApi', {
-  apiName: 'kash-contact-auth-api',
-  description: 'HTTP API for authentication and OTP verification',
-  corsPreflight: {
-    allowOrigins: ['*'],
-    allowMethods: [CorsHttpMethod.GET, CorsHttpMethod.POST, CorsHttpMethod.OPTIONS],
-    allowHeaders: ['Content-Type', 'Authorization', 'X-Amz-Date', 'X-Api-Key', 'X-Amz-Security-Token'],
-    maxAge: 300,
-  },
-});
-
-// Create Lambda integration
-const authIntegration = new HttpLambdaIntegration(
-  'AuthIntegration',
-  backend.functions.auth.resources.lambda
-);
-
-// Add routes
-httpApi.addRoutes({
-  path: '/auth/{proxy+}',
-  methods: [HttpMethod.POST, HttpMethod.OPTIONS],
-  integration: authIntegration,
-});
-
-// Output the API URL
-backend.addOutput({
-  custom: {
-    httpApiUrl: httpApi.url,
-  },
-});
+// Amplify Gen 2 backend definition complete
+// The Lambda function is defined in amplify/functions/resource.ts
+// VPC configuration will be applied via AWS Console after initial deployment
+// (Amplify's CDK integration doesn't support complex VPC lookups in this context)
 
