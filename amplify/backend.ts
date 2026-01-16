67aee4794aa6bcd0e61ea5fab50cc2cb3e14a7b9
MASINGITA OTTIS MALULEKE <216135982@edu.vut.ac.za>
2026-01-16 14:11:00 +0200

    Fix Amplify function deployment with proper VPC and IAM configuration

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -5,6 +5,8 @@ import { functions } from './functions/resource';
 import { Stack } from 'aws-cdk-lib';
 import { HttpApi, HttpMethod, CorsHttpMethod } from 'aws-cdk-lib/aws-apigatewayv2';
 import { HttpLambdaIntegration } from '@aws-cdk/aws-apigatewayv2-integrations-alpha';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -14,6 +16,56 @@ const backend = defineBackend({
   data,
   functions,
 });
+
+// Get the auth Lambda function
+const authLambda = backend.functions.auth.resources.lambda;
+
+// Add VPC configuration to Lambda
+const vpc = ec2.Vpc.fromLookup(Stack.of(authLambda), 'ExistingVPC', {
+  vpcId: 'vpc-09a5094b22e709eef',
+});
+
+const securityGroup = ec2.SecurityGroup.fromSecurityGroupId(
+  Stack.of(authLambda),
+  'ExistingSecurityGroup',
+  'sg-0655ac897b884d2ce'
+);
+
+// Add VPC config
+authLambda.addToRolePolicy(new iam.PolicyStatement({
+  effect: iam.Effect.ALLOW,
+  actions: [
+    'ec2:CreateNetworkInterface',
+    'ec2:DescribeNetworkInterfaces',
+    'ec2:DeleteNetworkInterface',
+    'ec2:AssignPrivateIpAddresses',
+    'ec2:UnassignPrivateIpAddresses'
+  ],
+  resources: ['*'],
+}));
+
+// Add Secrets Manager permissions
+authLambda.addToRolePolicy(new iam.PolicyStatement({
+  effect: iam.Effect.ALLOW,
+  actions: [
+    'secretsmanager:GetSecretValue',
+    'secretsmanager:DescribeSecret'
+  ],
+  resources: [
+    'arn:aws:secretsmanager:us-east-1:357229249466:secret:my-app/db-credentials-*',
+    'arn:aws:secretsmanager:us-east-1:357229249466:secret:my-app/jwt-secret-*'
+  ],
+}));
+
+// Add SES permissions
+authLambda.addToRolePolicy(new iam.PolicyStatement({
+  effect: iam.Effect.ALLOW,
+  actions: [
+    'ses:SendEmail',
+    'ses:SendRawEmail'
+  ],
+  resources: ['*'],
+}));
 
 // Create HTTP API Gateway for auth endpoints
 const apiStack = backend.createStack('http-api-stack');
